d8ad965a39a270b0f6f3d59c3152a92edd4e6d59
markok3 <marko.angelovski2000@yahoo.com>
2026-09-07 22:02:38 +0200

    Use lowercase allchat in email sender header

--- ads/src/main/java/com/example/adsportalbe/services/impl/MailServiceImpl.java
+++ ads/src/main/java/com/example/adsportalbe/services/impl/MailServiceImpl.java
@@ -25,7 +25,7 @@ public class MailServiceImpl implements MailService {
     private final JavaMailSender mailSender;
     private final TemplateEngine templateEngine;
 
-    private final String FROM_NAME = "allChat Team";
+    private final String FROM_NAME = "allchat Team";
 
     @Value("${app.FRONT_END.URL}")
     private String FRONT_END_URL;
